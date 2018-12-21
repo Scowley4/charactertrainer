ea1f45f8b71d60831ff4d7540d66d15120948868
Stephen Cowley <stephencowley44@gmail.com>
2018-12-21 14:49:12 -0700

    Added generators

--- character_counter.py
+++ character_counter.py
@@ -5,6 +5,7 @@ import sys
 import time
 import re
 
+from tqdm import tqdm
 import jieba
 from zhon import hanzi
 from punctuation import CHIN_PUNC
@@ -13,9 +14,16 @@ import string
 # os.walk
 # dirpath, dirnames, filenames
 
+words_to_add = ['阿们', '定额组', '弟兄姐妹们', '弟兄姊妹们']
+for word in words_to_add:
+    jieba.add_word(word)
+
 SKIP = (CHIN_PUNC + string.whitespace +
         string.digits + string.punctuation +
         string.ascii_letters)
+
+CHAR_PATTERN = re.compile(f'[{hanzi.characters}]')
+SENT_PATTERN = re.compile(r'[0-9\-'+hanzi.sentence[1:])
 
 class Document:
     def __init__(self, text, filename=None):
@@ -23,16 +31,43 @@ class Document:
         self.filename = filename
 
         self.char_counts = Counter(t for t in text if t not in SKIP)
+        self.char_set = set(char for char in self.char_counts)
         self.total_char_count = sum(self.char_counts.values())
-        self.word_counts = Counter(t for t in jieba.cut(text, cut_all=False) if t not in SKIP)
-        self.sentences = re.findall(hanzi.sentence, text)
 
+        self.words = []
+        for word in jieba.cut(text, cut_all=False):
+            if word in SKIP:
+                continue
+            if len(word) == len(CHAR_PATTERN.findall(word)):
+                self.words.append(word)
+        self.word_counts = Counter(self.words)
+        self.word_set = set(word for word in self.word_counts)
+        self.total_word_count = sum(self.word_counts.values())
+
+        self.sentences = [s for s in SENT_PATTERN.findall(text) if len(s)>5]
+
+        self.ngrams = {}
+
+    def get_ngram_model(self, n, save=True):
+        ngram = self.ngrams.get(n)
+        if not ngram:
+            ngram = self._compute_ngram(n)
+            if save:
+                self.ngrams[n] = ngram
+        return ngram
+
+    def _compute_ngram(self, n):
+        ngram_counts = Counter()
+        for i in range(len(self.words)-n):
+            ngram = self.words[i:i+n]
+            ngram_counts[' '.join(ngram)] += 1
+        return ngram_counts
 
     @classmethod
     def from_file(cls, filename):
         with open(filename, 'r') as infile:
             text = infile.read()
-        return Document(text, filename)
+        return cls(text, filename)
 
     def __str__(self):
         f = self.filename if self.filename else 'text'
@@ -40,22 +75,94 @@ class Document:
 
 class Corpus:
     def __init__(self, filenames):
-        self.documents = tuple(Document.from_file(f) for f in filenames)
-        self.char_counts = sum((d.char_counts for d in self.documents), Counter())
+        print('Creating Documents')
+        self.documents = tuple(Document.from_file(f) for f in tqdm(filenames))
 
-        self.word_counts = sum((d.word_counts for d in self.documents), Counter())
+        print('Counting characters')
+        self.char_counts = Counter()
+        for d in tqdm(self.documents):
+            self.char_counts.update(d.char_counts)
+
+        print('Counting words')
+        self.word_counts = Counter()
+        for d in tqdm(self.documents):
+            self.word_counts.update(d.word_counts)
+
+        print('Counting sentences')
+        self.sentence_counts = Counter(s for doc in tqdm(self.documents)
+                                       for s in doc.sentences)
+
+        self.ngrams = {}
+
+        self.total_chars = sum(self.char_counts.values())
+        self.total_words = sum(self.word_counts.values())
+        self.total_sentences = sum(self.sentence_counts.values())
+
+    def get_ngram_model(self, n, save=True, save_doc=True):
+        ngram = self.ngrams.get(n)
+        if not ngram:
+            ngram = self._compute_ngram(n, save_doc=save_doc)
+            if save:
+                self.ngrams[n] = ngram
+        return ngram
+
+    def _compute_ngram(self, n, save_doc=True):
+        ngram = Counter()
+        for doc in tqdm(self.documents):
+            ngram.update(doc.get_ngram_model(n, save=save_doc))
+        return ngram
+
+
+def get_token_vs_type_counts(corpus):
+    token_counts = [0]
+    type_counts = [0]
+    types = set()
+    tmp = 0
+    for doc in corpus.documents:
+        for token in doc.text:
+            if token in SKIP:
+                continue
+            if token in types:
+                tmp += 1
+            else:
+                types.add(token)
+                token_counts.append(token_counts[-1] + tmp + 1)
+                type_counts.append(type_counts[-1]+1)
+                tmp = 0
+    return token_counts, type_counts
+
+
+def get_cutoff(corpus, cutoff=.8):
+    types = []
+    type_count = 0
+    token_count = 0
+    total = sum(corpus.char_counts.values())
+    common = iter(corpus.char_counts.most_common())
+    while (token_count/total) < cutoff:
+        char, count = next(common)
+        token_count += count
+        types.append(char)
+        type_count += 1
+    print(f'With {type_count} types, can read '
+          f'{token_count}/{total} tokens'
+          f'--{cutoff}')
+    return types, type_count
+
+
 
 
 def get_filenames(root_dir='./talks/'):
     all_filenames = []
     for dirpath, dirnames, filenames in os.walk(root_dir):
-        all_filenames += [os.path.join(dirpath, f) for f in filenames]
+        all_filenames += [os.path.join(dirpath, f) for f in filenames 
+                          if not f.endswith('.swp')]
     return all_filenames
 
 
 if __name__ == '__main__':
-    filenames = get_filenames()
-    corpus = Corpus(filenames)
+    filenames = get_filenames()[:50]
+    #doc = Document.from_file(filenames[0])
+    #corpus = Corpus(filenames)
 
 
 
--- page_scraper.py
+++ page_scraper.py
@@ -3,9 +3,12 @@ from bs4 import BeautifulSoup
 
 import time
 import random
+import re
 
 import sys
 import os
+
+from hanziconv import HanziConv
 
 BASE_URL = 'https://www.lds.org'
 DIRECTORY = 'talks'
@@ -15,11 +18,19 @@ os.makedirs(DIRECTORY, exist_ok=True)
 
 
 class Talk:
-    def __init__(self, title, author, author_role, body):
-        self.title = title
-        self.author = author
-        self.author_role = author_role
-        self.body = body
+    def __init__(self, title, author, author_role, body, form='simplified'):
+        if form == 'simplified':
+            self.title = HanziConv.toSimplified(title)
+            self.author = HanziConv.toSimplified(author)
+            self.author_role = HanziConv.toSimplified(author_role)
+            self.body = HanziConv.toSimplified(body)
+        elif form == 'traditional':
+            self.title = HanziConv.toTraditional(title)
+            self.author = HanziConv.toTraditional(author)
+            self.author_role = HanziConv.toTraditional(author_role)
+            self.body = HanziConv.toTraditional(body)
+        else:
+            raise ValueError(f'Unrecongnized form: {form}')
 
     def write(self, directory='.', prefix=''):
         filename = prefix + self.title + '-' + self.author
@@ -45,21 +56,39 @@ def get_text_if_has(soup, tag, class_=None, id_=None, default=None):
 next_link = '/languages/zhs/content/general-conference/2017/10/the-plan-and-the-proclamation'
 url = BASE_URL + next_link
 
-conference_urls = ['https://www.lds.org/languages/zhs/item/general-conference/2018/04',
-                   'https://www.lds.org/languages/zhs/item/general-conference/2017/10',
-                   'https://www.lds.org/languages/zhs/item/general-conference/2017/04',
-                   ]
+conference_urls = [
+        'https://www.lds.org/languages/zhs/item/general-conference/2018/04',
+        'https://www.lds.org/languages/zhs/item/general-conference/2017/10',
+        'https://www.lds.org/languages/zhs/item/general-conference/2017/04',
+        'https://www.lds.org/languages/zhs/item/general-conference/2016/10',
+        'https://www.lds.org/languages/zhs/item/general-conference/2016/04',
+        'https://www.lds.org/languages/zhs/item/general-conference/2015/10',
+        'https://www.lds.org/languages/zhs/item/general-conference/2015/04',
+        'https://www.lds.org/languages/zhs/item/general-conference/2014/10',
+        'https://www.lds.org/study/general-conference/2014/04/welcome-to-conference?lang=zho',
+        'https://www.lds.org/study/general-conference/2013/10/welcome-to-conference?lang=zho',
+        'https://www.lds.org/study/general-conference/2013/04/welcome-to-conference?lang=zho',
+        'https://www.lds.org/study/general-conference/2012/10/welcome-to-conference?lang=zho',
+        'https://www.lds.org/study/general-conference/2012/04/as-we-gather-once-again?lang=zho',
+        'https://www.lds.org/study/general-conference/2011/04/its-conference-once-again?lang=zho',
+        'https://www.lds.org/study/general-conference/2010/04/welcome-to-conference?lang=zho',
+        ]
 
+url = 'https://www.lds.org/languages/zho/item/general-conference/2014/04'
 def get_all_conference():
     conference_urls = [
-                       'https://www.lds.org/languages/zhs/item/general-conference/2016/10',
-                       'https://www.lds.org/languages/zhs/item/general-conference/2016/04',
-                       'https://www.lds.org/languages/zhs/item/general-conference/2015/10',
-                       'https://www.lds.org/languages/zhs/item/general-conference/2015/04',
-                       'https://www.lds.org/languages/zhs/item/general-conference/2014/10',
-                      ]
+        'https://www.lds.org/study/general-conference/2012/10/welcome-to-conference?lang=zho',
+        'https://www.lds.org/study/general-conference/2012/04/as-we-gather-once-again?lang=zho',
+        'https://www.lds.org/study/general-conference/2011/04/its-conference-once-again?lang=zho',
+        'https://www.lds.org/study/general-conference/2010/04/welcome-to-conference?lang=zho',
+        ]
+
+    re_year = re.compile(r'/(\d{4})/')
+    re_month = re.compile(r'/(04|10)')
     for url in conference_urls:
-        directory = url[-7:-3] + '-' + url[-2:]
+        year = re_year.findall(url)[0]
+        month = re_month.findall(url)[0]
+        directory = f'{year}-{month}'
         get_general_conference(url, directory)
 
 def get_general_conference(start_url, directory=''):
@@ -172,6 +201,7 @@ def parse_general_conference(soup):
 
     header = f'{title}\n\n{author}\n{author_role}\n\n'
     talk['body'] = header + '\n\n'.join(paragraphs)
+
     return Talk(**talk)
 # paragraphs = [p.text for p in body_block.findAll('p')]
 
